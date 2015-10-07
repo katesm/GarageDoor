b6fbeea17b1556aef8094958847d513ac14eb1d0
Katesm <michael.keith.kates@gmail.com>
2015-10-07 12:59:44 +0000

    Adding files to repository

---
+++ CherryServer.py
@@ -0,0 +1,132 @@
+﻿import cherrypy, os, os.path, string, time, json, threading
+from SimpleMail import Mail
+from datetime import datetime
+import RPi.GPIO as GPIO
+
+
+class PiServer(object):
+    def __init__(self,door, **kwargs):
+        '''Server applicaiton holds door object'''
+
+        self.door = door
+        cherrypy.engine.subscribe(channel = 'stop', callback= self.__stop)
+
+    def __stop(self):
+        self.door.monitor = False
+
+
+    @cherrypy.expose
+    def index(self):
+        return open('index.html')
+
+    #This GET method is polled to check the status of the door, if opened or closed
+    @cherrypy.expose
+    def doorStatus(self):
+        return str(self.door.state)
+    #Action to open or close the door
+    @cherrypy.expose
+    def doorAction(self, state):
+        if state == "True":
+            self.door.open()
+        if state == "False":
+            self.door.close()
+
+        return str(self.door.state)
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def doorLog(self):
+        '''Returns the in memory copy of times the door was opened'''
+
+        log = [self.door.time_open, self.door.time_closed]
+        return log
+
+
+
+class Door(object):
+    def __init__(self,config, **kwargs):
+
+        self.mailer = Mail(config)
+        #True the door is open, False it's closed
+        self.state = False
+        #List of times the door was opened and closed
+        self.time_open = []
+        self.time_closed = []
+
+        #Input pins for door
+        self.DOOR_PIN = config['doorPin']
+        self.RELAY_PIN = config['relayPin']
+
+
+
+        #Start the new thread up
+        self.monitor = True
+        self.monitor_thread = threading.Thread(target=self.__monitor_door)
+        #self.monitor_thread.setDaemon(True)
+        self.monitor_thread.start()
+
+
+
+
+
+
+    def __monitor_door(self):
+        '''Monitors the state of the door every second'''
+
+        GPIO.cleanup()
+          #Set GPIO board up to use basic numbering
+        GPIO.setmode(GPIO.BOARD)
+        #Set as input PIN with pull up resistor sense this is just a switch
+        GPIO.setup(self.DOOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #Setup for relay
+        GPIO.setup(self.RELAY_PIN,GPIO.OUT)
+
+        print("I'm another thread")
+        while self.monitor:
+           if GPIO.input(self.DOOR_PIN):
+              #Door is open
+              if not self.state:
+                  self.state = True
+                  self.time_open.append(datetime.now())
+                 # self.mailer.send("Door is open", "Door monitor")
+                  print("Door open")
+           else:
+               #The door is closed
+               if self.state:
+                   self.state = False
+                   self.time_closed.append(datetime.now())
+                   print("Door shut")
+
+           time.sleep(1)
+
+
+    #Open door - my relay is active low so no voltage to input pin cause relay to come on
+    def open(self):
+        GPIO.output(self.RELAY_PIN, False)
+    #Close the door
+    def close(self):
+        GPIO.output(self.RELAY_PIN,True)
+
+
+
+
+
+      
+       
+
+ # load config
+def load():
+    with open('config.json') as f:
+        return json.load(f)
+
+
+config = load()
+garage_door = Door(config)
+
+
+
+ #Start the server
+ #This is a blocking call
+cherrypy.config.update({'server.socket_host':'157.89.76.117'})
+cherrypy.quickstart(PiServer(garage_door))
+
---
+++ SimpleMail.py
@@ -0,0 +1,58 @@
+﻿from email.mime.text import MIMEText
+import smtplib
+import os
+import os.path
+
+class Mail(object):
+    '''This class is used for loading smtp settings config file and sending email
+       @param: config = dic obj whith the following
+       host = smtp server
+       port = smtp port number
+       msg_from = sender email address
+       msg_to = comma seprated list recipients
+       username = username for logging into server
+       password = your password
+       isGmail = Boolean 
+       requiresPassword = Boolean
+
+
+    '''
+    
+    def __init__(self, config, **kwargs):
+        #Set smtp server up
+        self.host = config['host']
+        self.port = config['port']
+        self.msg_to = config['smtp_to']
+        self.msg_from = config['smtp_from']
+        self.username = config['username']
+        self.password = config['password']
+        self.isGmail = config['isGmail'] == True
+        self.requiresPasssword = config['requiresPassword'] == True
+
+
+    def send(self, msg, subject):
+        '''Sends an email'''
+         # Add the gmail smtp server
+        server = smtplib.SMTP(host=self.host, port=self.port)
+        #if this is gmail call ttls method
+        if (self.isGmail): server.starttls()
+        # login
+        
+        try:
+            #Check if auth needed
+            if (self.requiresPasssword): server.login(self.username, self.password)
+            message = MIMEText(msg)
+            message["To"] = self.msg_to
+            message["From"] = self.msg_from
+            message["Subject"] = subject
+
+            print("Sending...")
+            server.sendmail(self.msg_from, self.msg_to, message.as_string())
+            print("Email was sent")
+            server.close()
+
+        except Exception as e:
+            print(e)
+            server.close()
+        
+
